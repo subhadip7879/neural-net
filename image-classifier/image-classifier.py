de661ab6a7fa4d2ed7b56f998bacd22d60ca956b
him1411 <himjan1416@gmail.com>
2017-07-30 03:46:17 +0530

    minor changes

--- image-classifier/image-classifier.py
+++ image-classifier/image-classifier.py
@@ -71,12 +71,20 @@ def new_conv_layer(input,num_input_channels, filter_size,num_filters,use_pooling
     shape = [filter_size, filter_size, num_input_channels, num_filters]
     weights = new_weights(shape=shape)
     biases = new_biases(length=num_filters)
-    layer = tf.nn.conv2d(input=input,filter=weights,strides=[1, 1, 1, 1],padding='SAME')
+    layer = tf.nn.conv2d(input=input,
+                         filter=weights,
+                         strides=[1, 1, 1, 1],
+                         padding='SAME')
+
     
     layer += biases
     
     if use_pooling:
-        layer = tf.nn.max_pool(value=layer, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],padding='SAME')
+        layer = tf.nn.max_pool(value=layer,
+                               ksize=[1, 2, 2, 1],
+                               strides=[1, 2, 2, 1],
+                               padding='SAME')
+
         layer = tf.nn.relu(layer)
         return layer, weights
 
@@ -102,10 +110,21 @@ x_image = tf.reshape(x, [-1, img_size, img_size, num_channels])
 y_true = tf.placeholder(tf.float32, shape=[None, 10], name='y_true')
 y_true_cls = tf.argmax(y_true, dimension=1)
 
-layer_conv1, weights_conv1 = new_conv_layer(input=x_image, num_input_channels=num_channels,filter_size=filter_size1, num_filters=num_filters1,use_pooling=True)
+
+layer_conv1, weights_conv1 = \
+    new_conv_layer(input=x_image,
+                   num_input_channels=num_channels,
+                   filter_size=filter_size1,
+                   num_filters=num_filters1,
+                   use_pooling=True)
 layer_conv1
 
-layer_conv2, weights_conv2 = new_conv_layer(input=layer_conv1, num_input_channels=num_filters1,filter_size=filter_size2,num_filters=num_filters2,use_pooling=True)
+layer_conv2, weights_conv2 = \
+    new_conv_layer(input=layer_conv1,
+                   num_input_channels=num_filters1,
+                   filter_size=filter_size2,
+                   num_filters=num_filters2,
+                   use_pooling=True)
 layer_conv2
 
 layer_flat, num_features = flatten_layer(layer_conv2)
@@ -113,21 +132,33 @@ layer_flat
 
 num_features
 
-layer_fc1 = new_fc_layer(input=layer_flat,num_inputs=num_features,num_outputs=fc_size,use_relu=True)
+In [34]:
+layer_fc1 = new_fc_layer(input=layer_flat,
+                         num_inputs=num_features,
+                         num_outputs=fc_size,
+                         use_relu=True)
 layer_fc1
 
-layer_fc2 = new_fc_layer(input=layer_fc1,num_inputs=fc_size,num_outputs=num_classes,use_relu=False)
+layer_fc2 = new_fc_layer(input=layer_fc1,
+                         num_inputs=fc_size,
+                         num_outputs=num_classes,
+                         use_relu=False)
 layer_fc2
 
 y_pred = tf.nn.softmax(layer_fc2)
 y_pred_cls = tf.argmax(y_pred, dimension=1)
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=layer_fc2, labels=y_true)
+
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=layer_fc2,
+                                                        labels=y_true)
 cost = tf.reduce_mean(cross_entropy)
+
 optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost)
 correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 session = tf.Session()
 session.run(tf.global_variables_initializer())
+
 train_batch_size = 64
 total_iterations = 0
 
@@ -224,7 +255,8 @@ def plot_conv_weights(weights, input_channel=0):
     for i, ax in enumerate(axes.flat):
         if i<num_filters:
             img = w[:, :, input_channel, i]
-            ax.imshow(img, vmin=w_min, vmax=w_max, interpolation='nearest', cmap='seismic')
+            ax.imshow(img, vmin=w_min, vmax=w_max,
+                      interpolation='nearest', cmap='seismic')
             
         ax.set_xticks([])
         ax.set_yticks([])
